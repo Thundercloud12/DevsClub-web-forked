2b52805eb6fd5a76e660a2db1262512cda81a261
Keerthan <poojarykeerthan163@gmail.com>
2026-07-21 20:51:12 +0530

    feat: add initialization delay to sign-in page to prevent Firebase auth rate limiting

--- app/pages/auth/signin.vue
+++ app/pages/auth/signin.vue
@@ -231,10 +231,33 @@
             </UiButton>
             <UiButton
               class="w-1/2"
-              :disabled="forgotLoading || !forgotEmail"
+              :disabled="forgotLoading || isInitializing || !forgotEmail"
               @click="handleForgotPassword"
             >
-              <template v-if="forgotLoading">
+              <template v-if="isInitializing">
+                <svg
+                  class="animate-spin -ml-1 mr-2 h-4 w-4 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Preparing...
+              </template>
+              <template v-else-if="forgotLoading">
                 <svg
                   class="animate-spin -ml-1 mr-2 h-4 w-4 text-white"
                   xmlns="http://www.w3.org/2000/svg"
@@ -301,6 +324,9 @@ const forgotLoading = ref(false)
 const forgotError = ref('')
 const isOfficialUser = ref(false)
 const forgotSuccess = ref('')
+// 1.5s delay to allow Firebase reCAPTCHA/risk engine to initialize,
+// preventing auth/quota-exceeded (429) on the get-oob-code endpoint
+const isInitializing = ref(false)
 
 const openForgotModal = () => {
   showForgotModal.value = true
@@ -308,6 +334,10 @@ const openForgotModal = () => {
   forgotError.value = ''
   forgotSuccess.value = ''
   isOfficialUser.value = false
+  isInitializing.value = true
+  setTimeout(() => {
+    isInitializing.value = false
+  }, 1500)
 }
 
 const handleForgotPassword = async () => {
